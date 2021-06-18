4cf407eeb3fe5e0d0381cf59addd48babc0df2d6
Akshat Tripathi <akshatofficial2019@gmail.com>
2021-06-18 10:41:54 +0530

    Deployed Successfully

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({baseURL: "http://localhost:5000"})
+const API = axios.create({baseURL: "https://memory-app-v1.herokuapp.com/"})
 
 //const fetchPostsURL = "http://localhost:5000/posts";
 
